4d048edf6647cd96ed172edaafc14ef6ba303a64
Смирнов Григорий <greg.smirnov2013@yandex.ru>
2021-03-26 23:44:56 +0300

    sfsafs

--- include/common.h
+++ include/common.h
@@ -2,14 +2,13 @@
 #ifndef INCLUDE_COMMON_H_
 #define INCLUDE_COMMON_H_
 
-typedef size_t coord_t;
 
 struct Pair {
-	coord_t x; // 0..N-1
-	coord_t y; // 0..M-1
+	unsigned x = 0;
+	unsigned y = 0;
 };
 
-const size_t N = 70;
-const size_t M = 150;
+const unsigned M = 50;
+const unsigned N = 50;
 
 #endif // INCLUDE_COMMON_H_
